9c4c2d28d21e5283f077b04439ef3297ba9d2729
DoubleHypercube <development@hypercube.gdn>
2026-09-16 05:32:31 +1200

    Revert "Brainwashing~ (#32)"
    This reverts commit f7063e2c13428c7c9bd3682f048b81d89fde2c53.

--- Content.Client/_HL/Brainwashing/BrainwasherSystem.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared._HL.Brainwashing;
-
-namespace Content.Client._HL.Brainwashing;
-
-// HL: Empty Client class so we can handle the client-side Verbs in SharedBrainwasherSystem
-public sealed class BrainwasherSystem : SharedBrainwasherSystem
-{
-
-}
